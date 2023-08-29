db135ca00414e2877a0b182c72830fccef104f78
Chhuong Le <chhuongle@gmail.com>
2023-08-29 11:29:30 -0700

    add bird onto canvas

--- flappybird.js
+++ flappybird.js
@@ -1,7 +1,23 @@
+//board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
+
+// bird
+let birdWidth = 34; // height to width ratio = 408/228 = 17/12
+let birdHeight = 24;
+// specify 2 parameters for x and y positions
+let birdPosX = boardWidth/8;
+let birdPosY = boardHeight/2;
+let birdImg;
+
+let bird = {
+  x: birdPosX,
+  y: birdPosY,
+  width: birdWidth,
+  height: birdHeight,
+}
 
 window.onload = function () {
   // assign variable board to css element board
@@ -10,4 +26,11 @@ window.onload = function () {
   board.width = boardWidth;
   // used for drawing on the board
   context = board.getContext("2d");
+
+  // load bird image
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
+  birdImg.onload = function() {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
+  }
 }
